65c6e6cbb94e9669d08a9a13cca43b3d1b523a2a
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2023-06-12 22:28:17 +0200

    Ajout de la classe Poster

--- src/Entity/Poster.php
+++ src/Entity/Poster.php
@@ -1,9 +1,54 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Entity;
 
+use Database\MyPdo;
+use Entity\Exception\EntityNotFoundException;
+use PDO;
+
 class Poster
 {
+    private int $id;
+    private string $jpeg;
 
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getJpeg(): string
+    {
+        return $this->jpeg;
+    }
+
+    /**
+     * @param int $id
+     * @return Poster
+     */
+    public static function findById(int $id): Poster
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+        SELECT id, jpeg
+        FROM image
+        WHERE id = :id
+        SQL
+        );
+        $stmt->execute([':id' => $id]);
+        $stmt ->setFetchMode(PDO::FETCH_CLASS, Poster::class);
+        $res = $stmt->fetch();
+
+        if ($res!==false) {
+            return $res;
+        }
+        throw new EntityNotFoundException("Aucun cover trouvé avec cet id");
+    }
 }
