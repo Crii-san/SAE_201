d202564c5d739d81133f3ddf479ff91007505798
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2023-06-12 21:41:46 +0200

    Structuration de la page avec la classe Webpage

--- public/index.php
+++ public/index.php
@@ -5,9 +5,19 @@ declare(strict_types=1);
 require_once '../vendor/autoload.php';
 
 use Database\MyPdo;
+use Html\WebPage;
 
 MyPDO::setConfiguration('mysql:host=mysql;dbname=souk0003_movie;charset=utf8', 'souk0003', 'Ouinouin2023');
 
+$webpage = new WebPage();
+
+#Titre de la page
+$webpage -> setTitle("Liste des films");
+
+#Header
+$webpage->appendContent("<h1>Films</h1>");
+
+#Content
 $stmt = MyPDO::getInstance()->prepare(
     <<<'SQL'
     SELECT id, title
@@ -18,5 +28,11 @@ SQL
 $stmt->execute();
 
 while (($ligne = $stmt->fetch()) !== false) {
-    echo "<p>{$ligne['title']}\n";
+    $webpage->appendContent("<p>{$ligne['title']}</p>\n");
 }
+
+#Footer
+$webpage->appendContent("<p>{$webpage->getLastModification()}</p>");
+
+
+echo $webpage->toHTML();
