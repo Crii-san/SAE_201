698b20fc312fd452f2100ed5dc3d0bbf6c821471
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2023-06-12 22:47:08 +0200

    Implémentation du programme poster

--- public/poster.php
+++ public/poster.php
@@ -1,2 +1,25 @@
 <?php
 declare(strict_types=1);
+
+require_once '../vendor/autoload.php';
+use Database\MyPdo;
+use Entity\Poster;
+use Entity\Exception\EntityNotFoundException;
+use Exception\ParameterException;
+
+MyPDO::setConfiguration('mysql:host=mysql;dbname=souk0003_movie;charset=utf8', 'souk0003', 'Ouinouin2023');
+
+try {
+    if (!isset($_GET['posterId']) || !ctype_digit($_GET['posterId'])) {
+        throw new ParameterException("Erreur");
+    }
+    $cover = Poster::findById((int)$_GET['posterId']);
+    header("Content-type: image/jpeg");
+    echo $cover->getJpeg();
+} catch (ParameterException) {
+    http_response_code(400);
+} catch (EntityNotFoundException) {
+    http_response_code(404);
+} catch (Exception) {
+    http_response_code(500);
+}
