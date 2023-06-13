521b4373617e948640bcbb3aa94e8756a51ea4b6
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2023-06-13 16:53:40 +0200

    Restructuration de la page movie et ajout de l'image

--- public/movie.php
+++ public/movie.php
@@ -10,12 +10,12 @@ use Entity\Actor;
 use Entity\Movie;
 use Entity\Exception\EntityNotFoundException;
 
-
-
 MyPDO::setConfiguration('mysql:host=mysql;dbname=souk0003_movie;charset=utf8', 'souk0003', 'Ouinouin2023');
 
+$movieId = intval($_GET['movieId']);
+
 $webPage = new WebPage();
-$movie = new Movie($_GET["originalLangue"],$_GET["originalTitle"],$_GET["overview"],$_GET["releaseDate"],$_GET["runtime"],$_GET["tagline"],$_GET["title"]);
+$movie = Movie::findById($movieId);
 
 #Titre de la page
 $webPage -> setTitle($movie->getTitle());
@@ -24,10 +24,10 @@ $webPage -> setTitle($movie->getTitle());
 $webPage->appendCssUrl("/css/style_movie.css");
 
 #Header
-$webPage->appendContent("<h1>{$movie->getTitle()} </h1>");
+$webPage->appendContent("<h1>Films - {$movie->getTitle()} </h1>");
 
 #content
-$poster=" "; #à completer
+$poster = $movie->getPosterId();
 $webPage->appendContent("<div>");
 $webPage->appendContent("<img src='/poster.php?posterId={$poster}'>");
 $webPage->appendContent("<p>{$movie->getTitle()} </p>");
@@ -39,28 +39,30 @@ $webPage->appendContent("<\div>");
 
 
 # à completer, implementer la variable dans le where
+/*
 $stmt = MyPDO::getInstance()->prepare(
     <<<'SQL'
     SELECT role, name,birthday,deathDay,biography,placeOfBirth
-    FROM people, cast 
-    WHERE movieID = [#insert actor id]
+    FROM people p, cast c, movie m
+    WHERE p.peopleId = c.peopleId
+    AND c.movieId = m.movieId
     ORDER BY orderIndex
 SQL
 );
 
-
 $stmt->execute();
 
 while (($ligne = $stmt->fetch()) !== false) {
-    $poster = ""; #Code à compléter
+    $vignette = ""; #Code à compléter
     $webPage->appendContent("<a href='actor.php?name={$ligne['name']}&?birthplace={$ligne['placeOfBirth']}&?birthdate={$ligne['birthday']}&?biography={$ligne['biography']}&?deathDay={$ligne['deathDay']}'><div>");
     $webPage->appendContent("<img src='/poster.php?posterId={$poster}'>");
     $webPage->appendContent("<p>{$ligne['role']}</p>\n");
     $webPage->appendContent("<p>{$ligne['name']}</p>\n");
     $webPage->appendContent("</div></a>");
 }
-
+*/
 #Footer
+/*
 $webPage->appendContent("<p>{$webPage->getLastModification()}</p>");
-
+*/
 echo $webPage->toHTML();
