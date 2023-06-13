c901a20dd12454adcba6f072f76734dd5a9c101d
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2023-06-13 23:28:44 +0200

    Ajout de sélecteurs CSS

--- public/movie.php
+++ public/movie.php
@@ -26,18 +26,29 @@ $webPage->appendContent("<h1 class='header'>Films - {$movie->getTitle()} </h1>")
 
 #content
 
+$webPage->appendContent("<div class='content'>");
+
+$webPage->appendContent("<div class='détailsFilm'>");
 # Film
 $poster = $movie->getPosterId();
-$webPage->appendContent("<div>");
-$webPage->appendContent("<img src='/poster.php?posterId={$poster}' alt='Affiche du film'>");
+$webPage->appendContent("<img class='affiche' src='/poster.php?posterId={$poster}' alt='Affiche du film'>");
+
+$webPage->appendContent("<div class='infosFilm'>");
+$webPage->appendContent("<div class='x'>");
 $webPage->appendContent("<p>Titre : {$movie->getTitle()} </p>");
 $webPage->appendContent("<p>Date de sortie : {$movie->getReleaseDate()} </p>");
-$webPage->appendContent("<p>Titre original : {$movie->getOriginalTitle()}</p>");
-$webPage->appendContent("<p>Slogan :{$movie->getTagline()} </p>");
+$webPage->appendContent("</div>");
+$webPage->appendContent("<p class='titreOriginal'>Titre original : {$movie->getOriginalTitle()}</p>");
+$webPage->appendContent("<p>Slogan : {$movie->getTagline()} </p>");
 $webPage->appendContent("<p>Résumé : {$movie->getOverview()} </p>");
+
+$webPage->appendContent("</div>");
+
 $webPage->appendContent("</div>");
 
 # Acteurs
+
+$webPage->appendContent("<div class='actors'>");
 
 $stmt = MyPDO::getInstance()->prepare(
     <<<'SQL'
@@ -54,20 +65,24 @@ $stmt->bindValue(':movieId', $movieId, PDO::PARAM_INT);
 $stmt->execute();
 
 while (($ligne = $stmt->fetch()) !== false) {
+
     $vignette = $ligne['avatarId'];
     $actorId = $ligne['id'];
 
     #lien vers l'acteur
     $webPage->appendContent("<a href='/actor.php?actorId={$actorId}'>");
-
+    $webPage->appendContent("<div class='actor'>");
     $webPage->appendContent("<img src='/poster.php?posterId={$vignette}' alt='Photo de l acteur'>");
+    $webPage->appendContent("<div class='roleActor'>");
     $webPage->appendContent("<p>Rôle : {$ligne['role']}</p>\n");
     $webPage->appendContent("<p>Acteur : {$ligne['name']}</p>\n");
-
+    $webPage->appendContent("</div>");
+    $webPage->appendContent("</div>");
     $webPage->appendContent("</a>");
 }
 
-
+$webPage->appendContent("</div>");
+$webPage->appendContent("</div>");
 #Footer
 $webPage->appendContent("<p class='footer'>Dernière mofication : {$webPage->getLastModification()}</p>");
 
