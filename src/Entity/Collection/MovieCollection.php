3148660c886cff3de10be56e1224e08e2a80238e
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2023-06-13 10:41:19 +0200

    Ajout de la méthode findAll

--- src/Entity/Collection/MovieCollection.php
+++ src/Entity/Collection/MovieCollection.php
@@ -3,7 +3,24 @@ declare(strict_types=1);
 
 namespace Entity\Collection;
 
+use Database\MyPdo;
+use Entity\Movie;
+use PDO;
+
 class MovieCollection
 {
-
+    /** Retourne un tableau contenant tous les artistes triés par ordre alphabétique
+     * @return Movie[]
+     */
+    public static function findAll() : array
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+    SELECT *
+    FROM movie
+    SQL
+        );
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_CLASS, Movie::class);
+    }
 }
