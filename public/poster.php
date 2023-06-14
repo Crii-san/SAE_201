ed07d9d7408b4bfae0f9e074085c3bfa2125cf64
font0087 <thomas.fontao@etudiant.univ-reims.fr>
2023-06-14 10:49:02 +0200

    ajout de la documentation de la page poster.php

--- public/poster.php
+++ public/poster.php
@@ -7,16 +7,22 @@ use Entity\Poster;
 use Entity\Exception\EntityNotFoundException;
 use Exception\ParameterException;
 
+# Connection à la base de donnée
 MyPDO::setConfiguration('mysql:host=mysql;dbname=souk0003_movie;charset=utf8', 'souk0003', 'Ouinouin2023');
 
+#verification de l'éxistance d'un poster pour l'id fournis
 try {
     if (!isset($_GET['posterId']) || !ctype_digit($_GET['posterId'])) {
         throw new ParameterException("Erreur");
     }
+    # initialisation du poster correspondant à l'id choisie
     $cover = Poster::findById((int)$_GET['posterId']);
     header("Content-type: image/jpeg");
+    #envois de l'image sous format jpeg
     echo $cover->getJpeg();
-} catch (ParameterException) {
+}
+#envois des exceptions
+catch (ParameterException) {
     http_response_code(400);
 } catch (EntityNotFoundException) {
     http_response_code(404);
